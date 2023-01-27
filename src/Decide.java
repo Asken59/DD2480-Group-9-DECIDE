aab4f2ba5391707f1bdf4bc166e1513884a1e284
Asken59 <michaelask@hotmail.se>
2023-01-27 11:11:33 +0100

    feat/2 Added code skeleton 	new file:   ../Decide.java 	new file:   ../Parameters_t.java 	new file:   DecideTests.java

---
+++ src/Decide.java
@@ -0,0 +1,35 @@
+public class Decide {
+    enum Connectors{NOTUSED, ORR, ANDD};
+
+    //All "global" variables
+    static Parameters_t PARAMETERS;
+
+    static double[] X;
+
+    static double[] Y;
+
+    static int NUMPOINTS;
+
+    static Connectors[][] LCM;
+
+    static Boolean[][] PUM;
+
+    static Boolean[] PUV;
+
+    static Boolean[] CMV;
+
+    static Boolean[] FUV;
+
+    static Boolean LAUNCH;
+
+    public static void DECIDE(){
+        //TODO: Evaluate LICS and generate the CMV
+        //TODO: Generate the PUM using the CMV and LCM
+        //TODO: Generate the FUV using the PUM and PUV
+        //TODO: Evaluate LAUNCH and print
+    }
+
+    public static Boolean LIC0(){
+        return false;
+    }
+}
