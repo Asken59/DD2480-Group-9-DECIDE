6144bece4f3415b13f09bc399531e9c1aab84eb8
Asken59 <michaelask@hotmail.se>
2023-01-27 12:30:31 +0100

    feat/10 Added LIC3 function 	modified:   Decide.java

--- src/Decide.java
+++ src/Decide.java
@@ -32,4 +32,16 @@ public class Decide {
     public static Boolean LIC0(){
         return false;
     }
+
+    public static Boolean LIC3(){ //Decide if the area of a triangle formed by 3 cons. points is greater than AREA1
+        double triArea = 0;
+        for(int i = 0; i < NUMPOINTS - 2; i++){ //Loop through consecutive points
+            triArea = ((X[i+1] - X[i])*(Y[i+2] - Y[i]) - (X[i+3] - X[i])*(Y[i+1] - Y[i]))/2; //Shoelace formula
+            triArea = Math.abs(triArea);
+            if(PARAMETERS.AREA1 < triArea){
+                return true;
+            }
+        }
+        return false;
+    }
 }
