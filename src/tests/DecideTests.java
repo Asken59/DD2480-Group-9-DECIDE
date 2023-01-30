2328f4889211e992588a518074260dcf72e5a532
Asken59 <michaelask@hotmail.se>
2023-01-30 03:38:44 +0100

    feat/9 Added true and false tests for LIC11 	modified:   src/tests/DecideTests.java

--- src/tests/DecideTests.java
+++ src/tests/DecideTests.java
@@ -36,4 +36,22 @@ public class DecideTests {
         Decide.Y = new double[] {2, 5};
         Assertions.assertFalse(Decide.LIC0());
     }
+
+    @Test
+    public void test_LIC11_true(){
+        Decide.NUMPOINTS = 5;
+        Decide.PARAMETERS.G_PTS = 2;
+        Decide.X = new double[] {1, 3, 3, 3, 2};
+        Decide.Y = new double[] {2, 5, 1, 1, 1};
+        Assertions.assertTrue(Decide.LIC11());
+    }
+
+    @Test
+    public void test_LIC11_false(){
+        Decide.NUMPOINTS = 5;
+        Decide.PARAMETERS.G_PTS = 2;
+        Decide.X = new double[] {1, 3, 3, 2, 4};
+        Decide.Y = new double[] {2, 5, 1, 1, 1};
+        Assertions.assertFalse(Decide.LIC11());
+    }
 }
