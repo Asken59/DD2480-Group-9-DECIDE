4e11fc4c2a19f46b8355ffe1873ec4498812e497
Asken59 <michaelask@hotmail.se>
2023-01-30 09:37:30 +0100

    feat/7 Added tests for LIC2 true and false 	modified:   src/tests/DecideTests.java

--- src/tests/DecideTests.java
+++ src/tests/DecideTests.java
@@ -21,4 +21,22 @@ public class DecideTests {
     public void testLIC0(){
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void test_LIC2_true(){
+        Decide.NUMPOINTS = 5;
+        Decide.X = new double[] {0, -1, 0, 1, 0};
+        Decide.Y = new double[] {0, 1, 0, 1, 0};
+        Decide.PARAMETERS.EPSILON = (Math.PI/2) - 0.1; //PI - EPSILON ~ above 90 degrees
+        Assertions.assertTrue(Decide.LIC2());
+    }
+
+    @Test
+    public void test_LIC2_false(){
+        Decide.NUMPOINTS = 5;
+        Decide.X = new double[] {0, -1, 0, 1, 0};
+        Decide.Y = new double[] {0, 1, 0, 1, 0};
+        Decide.PARAMETERS.EPSILON = (Math.PI/2) + 0.1; //PI - EPSILON ~ below 90 degrees
+        Assertions.assertFalse(Decide.LIC2());
+    }
 }
