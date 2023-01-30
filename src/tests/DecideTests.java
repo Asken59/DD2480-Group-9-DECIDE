7b67c112e766fdd9c618ba38a2c28e58348e3471
Asken59 <michaelask@hotmail.se>
2023-01-30 09:20:56 +0100

    feat/10 Added tests for LIC3 true and false 	modified:   src/tests/DecideTests.java

--- src/tests/DecideTests.java
+++ src/tests/DecideTests.java
@@ -21,4 +21,22 @@ public class DecideTests {
     public void testLIC0(){
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void test_LIC3_true(){
+        Decide.NUMPOINTS = 5;
+        Decide.X = new double[] {1, 0, 4, 4, 2};
+        Decide.Y = new double[] {1, 0, 0, 4, 2};
+        Decide.PARAMETERS.AREA1 = 7.9;
+        Assertions.assertTrue(Decide.LIC3());
+    }
+
+    @Test
+    public void test_LIC3_false(){
+        Decide.NUMPOINTS = 5;
+        Decide.X = new double[] {1, 0, 4, 4, 2};
+        Decide.Y = new double[] {1, 0, 0, 4, 2};
+        Decide.PARAMETERS.AREA1 = 8.1;
+        Assertions.assertFalse(Decide.LIC3());
+    }
 }
