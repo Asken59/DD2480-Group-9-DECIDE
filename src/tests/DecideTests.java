744f20f552600bbfffdac7ec455f3b0be16cca3d
BuildTools <gabchr@kth.se>
2023-01-31 11:40:18 +0100

    doc/46: Added natural language descriptions for LIC0, LIC9, and LIC10 test cases

--- src/tests/DecideTests.java
+++ src/tests/DecideTests.java
@@ -35,6 +35,14 @@ public class DecideTests {
         Assertions.assertFalse(Decide.PUM[1][2]);
     }
 
+    // LIC0: Test whether the condition evaluates to true when there exists
+    // at least one  set of two consecutive data points that are a distance
+    // greater than LENGTH1 apart.
+    //
+    // We add two points with a distance of approx. 2.24 length units apart,
+    // and set the parameter LENGTH1 to 1. Thus, the condition should evaluate to true.
+    //
+    // Expected output: true
     @Test
     public void test_LIC0_true() {
         Decide.NUMPOINTS = 2;
@@ -44,6 +52,14 @@ public class DecideTests {
         Assertions.assertTrue(Decide.LIC0());
     }
 
+    // LIC0: Test whether the condition evaluates to false when there exists
+    // NO set of two consecutive data points that are a distance
+    // greater than LENGTH1 apart.
+    //
+    // We add two points with a distance of approx. 2.24 length units apart,
+    // and set the parameter LENGTH1 to 10. Thus, the condition should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC0_false() {
         Decide.NUMPOINTS = 2;
@@ -216,9 +232,20 @@ public class DecideTests {
         Assertions.assertFalse(Decide.LIC8());
     }
 
+    // LIC9: Test whether the condition evaluates to true when there exists at least
+    // one set of three points separated by exactly C_PTS and D_PTS consecutive
+    // intervening points, respectively, and forms an angle lesser than (PI - EPSILON)
+    // OR greater than (PI + EPSILON).
+    //
+    // We add five points, and set both parameters C_PTS and D_PTS to 1.
+    // First, third and fifth points form an equilateral triangle.
+    // All angles are therefore PI/3 radians (60 degrees) and we set EPSILON to PI/2 (90 degrees)
+    // so that the condition should evaluate to true.
+    //
+    // Expected output: true
     @Test
     public void test_LIC9_true() {
-        // First, third and fifth points form an equilateral triangle. All angles are PI/3 radians (60 degrees).
+        //
         Decide.NUMPOINTS = 5;
         Decide.PARAMETERS.C_PTS = 1;
         Decide.PARAMETERS.D_PTS = 1;
@@ -228,6 +255,15 @@ public class DecideTests {
         Assertions.assertTrue(Decide.LIC9());
     }
 
+    // LIC9: Test whether the condition evaluates to false when there exists NO
+    // set of three points separated by... (see description above for LIC9 positive test)
+    //
+    // We add five points, and set both parameters C_PTS and D_PTS to 1.
+    // First, third and fifth points form an equilateral triangle.
+    // All angles are therefore PI/3 radians (60 degrees) and we set EPSILON to 5PI/6 (150 degrees)
+    // so that the condition should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC9_false() {
         // First, third and fifth points form an equilateral triangle. All angles are PI/3 radians (60 degrees).
@@ -240,12 +276,26 @@ public class DecideTests {
         Assertions.assertFalse(Decide.LIC9());
     }
 
+    // LIC9: Test whether the condition evaluates to false when passed invalid input.
+    //
+    // The condition cannot be fulfilled if NUMPOINTS is less than 5. Thus,
+    // we set the parameter NUMPOINTS to 4 so that the condition should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC9_false_2() {
         Decide.NUMPOINTS = 4;
         Assertions.assertFalse(Decide.LIC9());
     }
 
+    // LIC9: Test whether the condition evaluates to false when passed invalid input.
+    //
+    // The condition cannot be fulfilled if any of the two other points of the triangle
+    // (see comment for LIC9 positive test) coincide with the vertex point.
+    // Thus, we set the first point to coincide with the vertex point so that the condition
+    // should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC9_false_3() {
         Decide.NUMPOINTS = 5;
@@ -256,6 +306,14 @@ public class DecideTests {
         Assertions.assertFalse(Decide.LIC9());
     }
 
+    // LIC10: There exists at least one set of three data points separated by
+    // exactly E_PTS and F_PTS consecutive intervening points, respectively,
+    // that are the vertices of a triangle with area greater than AREA1.
+    //
+    // We form an equilateral triangle with an area of 6 area units, and we set
+    // the parameter AREA1 to 4, so that the condition should evaluate to true.
+    //
+    // Expected output: true
     @Test
     public void test_LIC10_true() {
         // Equilateral triangle with an area of 6 area units
@@ -268,6 +326,13 @@ public class DecideTests {
         Assertions.assertTrue(Decide.LIC10());
     }
 
+    // LIC10: There exists NO set of three data points separated
+    // by... (see comment above for LIC10 positive test case)
+    //
+    // We form an equilateral triangle with an area of 6 area units, and we set
+    // the parameter AREA1 to 10, so that the condition should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC10_false_1() {
         // Equilateral triangle with an area of 6 area units
@@ -280,12 +345,26 @@ public class DecideTests {
         Assertions.assertFalse(Decide.LIC10());
     }
 
+    // LIC10: Test whether the condition evaluates to false when passed invalid input.
+    //
+    // The condition cannot be met if the number of points are less than 5. Thus,
+    // we set the parameter NUMPOINTS to 4 so that the condition should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC10_false_2() {
         Decide.NUMPOINTS = 4;
         Assertions.assertFalse(Decide.LIC10());
     }
 
+    // LIC10: Test whether the condition evaluates to false when passed invalid input.
+    //
+    // The condition cannot be met if the number of consecutive intervening points (E_PTS+F_PTS)
+    // spare less than 3 points for the triangle (E_PTS+F_PTS > NUMPOINTS-3).
+    // We set the parameter NUMPOINTS to 5 (valid) and both E_PTS and F_PTS to 4 so that the
+    // condition should evaluate to false.
+    //
+    // Expected output: false
     @Test
     public void test_LIC10_false_3() {
         Decide.NUMPOINTS = 5;
